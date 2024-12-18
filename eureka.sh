46e5232a437929e9de1ae0b0e850b6e55902cc28
Janpstrunn <janpstrunn@particular.slmail.me>
2024-12-18 09:32:51 -0300

    eureka: Improve code readability
    Remade how colors are used to make code cleaner.

--- eureka.sh
+++ eureka.sh
@@ -2,12 +2,18 @@
 
 editor=${EDITOR:-vi}
 pager=${PAGER:-less}
-
 config="$HOME/.local/share/eureka.conf"
 
-if [ ! -f "$config" ]; then
-  echo "$(tput setaf 196)The configuration file isn't set!$(tput sgr0)"
-fi
+RED="$(tput setaf 196)"
+BLUE="$(tput setaf 87)"
+GREEN="$(tput setaf 82)"
+
+function text() {
+  local color=$1
+  local text=$2
+  local reset=$(tput sgr0)
+  echo -e "${color}${text}${reset}"
+}
 
 function help() {
 	printf "
@@ -29,30 +35,30 @@ function setup() {
 	if [ ! -f "$config" ]; then
     touch "$HOME/.local/share/eureka.conf"
 
-		echo "$(tput setaf 87)> Insert your remote repo address (e.g. https://github.com/$USER/eureka/$(tput sgr0)"
+		text "$BLUE" "> Insert your remote repo address (e.g. https://github.com/$USER/eureka/"
     read -p ">> " irepo
 
-		echo "$(tput setaf 87)> Select the directory you want to clone your repo to (e.g. /home/$USER)$(tput sgr0)"
-		echo "$(tput setaf 87)> The result will be: /home/$USER/your-repo-name$(tput sgr0)"
+		text "$BLUE" "> Select the directory you want to clone your repo to (e.g. /home/$USER)"
+		text "$BLUE" "> The result will be: /home/$USER/your-repo-name"
     read -p ">> " clone
-    echo "$(tput setaf 87)> Cloning your repo now...$(tput sgr0)"
+    text "$BLUE" "> Cloning your repo now..."
     git -C "$clone" clone "$irepo"
     if [ "$?" -eq 0 ]; then
-      echo "$(tput setaf 82)> You repo has been cloned to $clone$(tput sgr0)"
+      text "$GREEN" "> You repo has been cloned to $clone"
     else
-      echo "$(tput setaf 196)> An error occured! Try cloning manually$(tput sgr0)"
+      text "$RED" "> An error occured! Try cloning manually"
     fi
 
 		while true; do
-      echo "$(tput setaf 87)> Insert the name of your repo (e.g. eureka)$(tput sgr0)"
+      text "$BLUE" "> Insert the name of your repo (e.g. eureka)"
       read -p ">> " iname
-      echo "path = $clone/$iname" >> "$config" && echo "$(tput setaf 82)> The provided path has been saved to $config$(tput sgr0)"
+      echo "path = $clone/$iname" >> "$config" && text "$GREEN" "> The provided path has been saved to $config"
       exit 0
     done
 
   else
-    echo "$(tput setaf 82)> You already have a configuration file!$(tput sgr0)"
-    echo "$(tput setaf 82)> Do you want to overwrite it? (y/n):$(tput sgr0)"
+    text "$GREEN" "> You already have a configuration file!"
+    text "$GREEN" "> Do you want to overwrite it? (y/n):"
     read -p ">> " overwrite
     if [ "$overwrite" = "y" ]; then
       rm "$config"
@@ -64,13 +70,13 @@ function setup() {
 }
 
 function getidea() {
-	echo "$(tput setaf 87)> Idea Summary$(tput sgr0)"
+	echo "${BLUE}> Idea Summary"
 	read -p ">> " idea
-	echo "$(tput setaf 87)> Idea Content$(tput sgr0)"
+	echo "${BLUE}> Idea Content"
   read -p ">> " ideacontent
 }
 
-function dogit() {
+function git_cmd() {
 	git -C "$path" add .
 	git -C "$path" commit -m "$idea"
 	git -C "$path" push origin main
@@ -87,7 +93,7 @@ function fetch() {
 function checkpath() {
   path=$(grep "path" "$config" | awk -F ' = ' '{print $2}')
   if [ ! -d "$path" ]; then
-    echo "$(tput setaf 196)The path doesn't exist!$(tput sgr0)"
+    text "$RED" "The path doesn't exist!"
     exit 1
   fi
 }
@@ -99,33 +105,33 @@ function preview() {
 
 function target() {
   checkpath
-	echo "$(tput setaf 87)> Available files:$(tput sgr0)"
+	text "$BLUE" "> Available files:"
   find "$path" -type f -name '*.md' -printf '%P\n' | awk -F. '{print $1}'
-	echo "$(tput setaf 87)> Name your file$(tput sgr0)"
+	text "$BLUE" "> Name your file"
 	read -p ">> " filename
-	echo "$(tput setaf 87)> Summary$(tput sgr0)"
+	text "$BLUE" "> Summary"
 	read -p ">> " idea
   if [ ! -f "$path/$filename.md" ]; then
-    echo "$(tput setaf 196)> The $filename file doesn't exist!$(tput sgr0)"
-    echo "$(tput setaf 87)> Do you want to create one now? (y/n)$(tput sgr0)"
+    text "$RED" "> The $filename file doesn't exist!"
+    text "$BLUE" "> Do you want to create one now? (y/n)"
     read -p ">> " create
     if [ "$create" = "y" ]; then
-      echo "$(tput setaf 87)> Creating one now...$(tput sgr0)"
+      text "$BLUE" "> Creating one now..."
       touch "$path/$filename.md"
-      echo "$(tput setaf 82)> $filename.md created!$(tput sgr0)"
+      text "$GREEN" "> $filename.md created!"
     fi
   fi
 	"$editor" "$path/$filename.md"
-  dogit
+  git_cmd
 	exit 0
 }
 
 function editor() {
   checkpath
-	echo "$(tput setaf 87)> Idea Summary$(tput sgr0)"
+	text "$BLUE" "> Idea Summary"
 	read -p ">> " idea
 	"$editor" "$path/README.md"
-  dogit
+  git_cmd
 	exit 0
 }
 
@@ -145,9 +151,13 @@ function eureka() {
     fi
   fi
   sed -i "2a - $ideacontent" "$path/README.md"
-  dogit
+  git_cmd
 	exit 0
 }
+
+if [ ! -f "$config" ]; then
+  text "$RED" "The configuration file isn't set!"
+fi
 
 if [ "$1" = "" ]; then
 	eureka
